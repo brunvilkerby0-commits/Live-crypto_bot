d85f1978d8dd303ecc98810d934220c1f5257b9d
brunvilkerby0-commits <brunvilkerby0@gmail.com>
2026-04-29 07:35:05 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,1 @@
-const { connectDB } = require('./db');
 
-async function test() {
-    const db = await connectDB();
-    if (db) {
-        console.log("Koneksyon an mache 100%!");
-    } else {
-        console.log("Koneksyon an echwe.");
-    }
-}
-
-test();
