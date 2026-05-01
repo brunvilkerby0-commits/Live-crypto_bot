d0960606d90ca8b8ddae5fe661e0a0471c38c6c2
brunvilkerby0-commits <brunvilkerby0@gmail.com>
2026-04-30 22:33:22 -0400

    server.js

--- server.js
+++ server.js
@@ -1,2 +1,7 @@
-const cors = require('cors');
-app.use(cors());
+// Nan kòd server.js la, ranje pati sa a:
+const response = await axios.post('https://issuecards.api.bridgecard.co/v1/issuing/sandbox/cards/virtual', {
+    card_type: "visa",
+    card_name: "Live Crypto Bot Client", // Nou mete non pwojè w la
+    currency: "USD",
+    amount: 500 
+}, 
